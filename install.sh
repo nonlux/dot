234b5fda2e2578cb8830042a6e513afe5284c83a
nonlux <nonluxi@gmail.com>
2018-07-28 17:09:37 +0800

    install docker-compose

--- install.sh
+++ install.sh
@@ -16,3 +16,6 @@ sudo pacman -Sy tmux
 ln ~/src/dot/tmux/.tmux.conf ~/.tmux.conf
 rm ~/.spacemacs
 ln ~/src/dot/spacemacs/.spacemacs ~/.spacemacs
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
